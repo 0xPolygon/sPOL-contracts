f06ab3ad9954d925c76323206b8c35bf53782be5
ethyla <36268899+ethyla@users.noreply.github.com>
2025-10-27 19:23:14 +0100

    feat: add tunnel contracts and upgrade to 0.8

---
+++ src/msg/ChildTunnel.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.30;
+
+import {BaseChildTunnel} from "./BaseChildTunnel.sol";
+
+contract ChildTunnel is BaseChildTunnel {
+    constructor(address _stateSyncer) BaseChildTunnel(_stateSyncer) {}
+    function _processMessageFromRoot(bytes memory message) internal override {
+        // you can implement custom message processing logic here
+    }
+}
---
+++ src/msg/interfaces/IStateSender.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.8.30;
+
+interface IStateSender {
+    function syncState(address receiver, bytes calldata data) external;
+}
