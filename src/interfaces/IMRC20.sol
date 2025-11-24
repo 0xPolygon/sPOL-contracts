bd55b4d3fad606cd8de3966881d81f781a2e3efe
ethyla <36268899+ethyla@users.noreply.github.com>
2025-11-24 13:04:45 +0100

    feat: initial fork integration test

--- src/interfaces/IMRC20.sol
+++ src/interfaces/IMRC20.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.30;
 
 interface MRC20 {
+    event Withdraw(address indexed token, address indexed from, uint256 amount, uint256 input1, uint256 output1);
+
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 value) external payable returns (bool);
     function withdraw(uint256 amount) external payable;
